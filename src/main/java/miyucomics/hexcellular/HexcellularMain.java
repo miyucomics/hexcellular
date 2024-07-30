07466cb2bdb02f95c5d2f3a2737a6bd07ba681c0
miyucomics <nbriannyc@gmail.com>
2024-07-30 13:55:27 -0400

    Some preparations

--- src/main/java/miyucomics/hexcellular/HexcellularMain.java
+++ src/main/java/miyucomics/hexcellular/HexcellularMain.java
@@ -4,11 +4,10 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 
 public class HexcellularMain implements ModInitializer {
-	public static final String MOD_ID = "hexical";
+	public static final String MOD_ID = "hexcellular";
 
 	@Override
 	public void onInitialize() {
-		HexcellularPatterns.init();
 	}
 
 	public static Identifier id(String string) {
